86de2284d5bc85f078a97aa60ec39b827ef41a9d
Ji Woo <huynhztienzdat@gmail.com>
2022-01-25 10:51:10 +0700

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Calendar, LunarDate, SolarDate } from './model';
-
-export * from './constant';
-export * from './model';
+import { VEGETARIAN_DAY, WEEKDAY } from './constant';
 
 export default {
   Calendar,
   LunarDate,
   SolarDate,
+  WEEKDAY,
+  VEGETARIAN_DAY,
 }
