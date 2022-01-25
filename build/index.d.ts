efe88cab829c350b14e048346e61cbc73ad1a534
Ji Woo <huynhztienzdat@gmail.com>
2022-01-25 10:53:07 +0700

    fix: export

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,9 +1,9 @@
 import { Calendar, LunarDate, SolarDate } from './model';
-export * from './constant';
-export * from './model';
 declare const _default: {
     Calendar: typeof Calendar;
     LunarDate: typeof LunarDate;
     SolarDate: typeof SolarDate;
+    WEEKDAY: string[];
+    VEGETARIAN_DAY: number[];
 };
 export default _default;
